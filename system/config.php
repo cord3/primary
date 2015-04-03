002a4964774923db4b26a2170689c8ab9c379347
root <Rob Gloginja>
2015-04-04 03:50:45 +1100

    Initial work on 'system' has started with the creation of 'system/config.php' allowing the changing of variables that effect Cord3.
    error.html has also been created in case of a MySQL error.
    
    Changes to css/custom.css were for the Transparent button
    class which we use on error.html
    
     Changes to be committed:
    	modified:   TODO.md
    	modified:   cord3.html
    	modified:   css/custom.css
    	new file:   error.html
    	new file:   system/config.php

---
+++ system/config.php
@@ -0,0 +1,23 @@
+<?php
+	# This is the config file for the Cord3 'system'
+	# You will be able to edit just about ANY setting in Cord3
+	
+	# General App Settings
+	$appName = "Cord3";
+	$appTitle = "Manage Anything. Manage Everything.";
+	$appEnabled = True;
+	
+	# Database Settings
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'G*********!');
+	define('DB_NAME', 'cord3_primary');
+	
+	# Connect to the database
+	try {
+		$link = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die ('<META HTTP-EQUIV="Refresh" CONTENT="0;URL=../error.html">');
+		$db = mysql_select_db(DB_NAME, $link);
+	} catch (Exception $e) {
+		$ad = "dd";
+	}
+?>
